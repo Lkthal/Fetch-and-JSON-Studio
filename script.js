7ae52c4f7f51453f7a308de25fa8cc8cb3f04b74
Thal <thal.syhabout@harlowconsultinggroup.com>
2022-08-11 18:23:40 -0700

    studio without bonus mission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // TODO: add code here
+
+function init(){
+  //TODO: fetch JSON
+  const astronauts = "https://handlers.education.launchcode.org/static/astronauts.json";
+  fetch(astronauts).then(function(response){
+    response.json().then(function(json){
+      const div = document.getElementById("container");
+      for(let i = 0; i<json.length; i++){
+        div.innerHTML +=
+        `
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${json[i].firstName, json[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${json[i].hoursInSpace}</li>
+        <li>Active: ${json[i].active}</li>
+        <li>Skills: ${json[i].skills}</li>
+        </ul>
+        </div>
+        <img class="avatar" src = ${json[i].picture}
+        </div>
+        `;
+      }
+    });
+  });
+}
+
+window.addEventListener("load", init);
